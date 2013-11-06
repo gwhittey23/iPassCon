b27dabf70b5618b18b0159c4b94451519600d008
Gerard Whittey <gerard.whittey@gmail.com>
2013-11-06 08:48:43 -0500

    cleaned up code

--- apps/management/commands/csv_students.py
+++ apps/management/commands/csv_students.py
@@ -28,97 +28,58 @@ class Command(BaseCommand):
             
             # set CVS out file files
             csv_output_file = 'csv_output/csv_student1.csv'
-            print csv_output_file
             csv_header = 'csv_input/csv_student_header.txt'
+            #use header_file to fill in csv header from csv_header
             header_file = open(csv_header, 'r')
             csv_header = csv.reader(header_file, delimiter=',', quotechar='"')
             header = csv_header.next()
             header_file.close()
+            #open csv outpu file
             outfile = open(csv_output_file, "wb")
             my_writer = csv.writer(outfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
             my_writer.writerow(header)
+            #get all students from student table except graldelevel 06
             student_data = Student.objects.all().order_by('studentid').exclude(gradelevel='06')
+            #go threw each student and gather info
             for counter, a_student in enumerate(student_data):
                 print "The Counts is %s of %i" % (counter, len(student_data))
-                #print a_student.studentid
-                mother_full_name = ''
-                mother_phone_day = ''
-                mother_phone_home = ''
-                father_full_name = ''
-                father_phone_day = ''
-                father_phone_home = ''
-                a_guardianship = ''
-                guardian_fn = ''
-                guardian_ln = ''
-                a_student_race = ''
-                emerg1_contact_name = ''
-                emerg1_relationship = ''
-                emerg1_phone = ''
-                emerg1_ptype = ''
-                emerg1_phone_number = ''
-                emerg2_contact_name = ''
-                emerg2_relationship = ''
-                emerg2_phone = ''
-                emerg2_ptype = ''
-                emerg2_phone_number = ''
-                emerg3_contact_name = ''
-                emerg3_relationship = ''
-                emerg3_phone = ''
-                emerg3_ptype = ''
-                emerg3_phone_number = ''
-                school_number = ''
-                enroll_status = ''
-                Sched_Scheduled = 0
-                entry_sch_date = ''
-                try:
+                #here we are just setting all values so they will not be used from loop to loop
+                sched_scheduled = 0
+                mother_full_name, mother_phone_day, mother_phone_home, father_full_name, father_phone_day, \
+                    father_phone_home, a_guardianship, guardian_fn, guardian_ln, a_student_race, emerg1_contact_name, \
+                    emerg1_relationship, emerg1_phone, emerg1_ptype, emerg1_phone_number, emerg2_contact_name, \
+                    emerg2_relationship, emerg2_phone, emerg2_ptype, emerg2_phone_number, emerg3_contact_name, \
+                    emerg3_relationship, emerg3_phone, emerg3_ptype, emerg3_phone_number, school_number, enroll_status, \
+                    entry_sch_date = ''
+
+                try: # get student race info
                     a_stuethnicx = Stuethnicx.objects.filter(studentid=a_student.studentid)[
                                    :1].get()  # student ethnicity
                     a_student_race = a_stuethnicx.ethnicracecodesseq.ethniccode
                 except Stuethnicx.DoesNotExist:
                     write_error(error_file, "a_stuethnicx", a_student.studentid)
 
-                    print '*********'
-                    print "a_stuethnicx"
-                    print a_student.studentid
-                    print '*********'
-                try:
+                try: # see if student is in the addressperson db and pick only student  address type
                     student_address = Addressperson.objects.filter(studentid=a_student.studentid, addresstypeseq=2)[
                                       :1].get()
                 except Addressperson.DoesNotExist:
                     write_error(error_file, "student_address", a_student.studentid)
-                    print '*********'
-                    print "student_address"
-                    print a_student.studentid
-                    print '*********'
-                try:
+
+                try: # get student mailing address
                     mailing_address = Addressperson.objects.filter(studentid=a_student.studentid, addresstypeseq=5)[
                                       :1].get()
                 except Addressperson.DoesNotExist:
                     write_error(error_file, "mailing_address", a_student.studentid)
-                    print '*********'
-                    print 'mailing_address'
-                    print a_student.studentid
-                    print '*********'
-                    # have to join address from iPass address1,address2,address3 to 1 address field for powerschool
-                    #mailing_address_street() does this.
+                # have to join address from iPass address1,address2,address3 to 1 address field for powerschool
+                #mailing_address_street() does this.
                 mailing_address_street = get_full_address(mailing_address)
                 home_address_street = get_full_address(student_address)
-                try:
+
+                try: # get student home phone
                     home_phone = Phoneperson.objects.filter(studentid=a_student.studentid, phonetypeseq=1)[:1].get()
                 except Phoneperson.DoesNotExist:
                     write_error(error_file, "home_phone", a_student.studentid)
-                    print '*********'
-                    print 'home_phone'
-                    print a_student.studentid
-                    print '*********'
-                    # need to add in 'code' for all !---xxxx---! items added in as placeholder
-                try:
-                    entry_date = Entrywithdrawl.objects.filter(studentid=a_student.studentid) \
-                        [:1].get().yearentrydate
-                except Entrywithdrawl.DoesNotExist:
-                    entry_date = ''
-                    write_error(error_file, "entry_date", a_student.studentid)
-                try:
+                try:  # get father info
                     a_father = Guardianstudent.objects.filter(studentid=a_student.studentid, relationshipseq=1)[
                                :1].get()
                     father_full_name = a_father.personseq.firstname + ' ' + a_father.personseq.lastname
@@ -130,7 +91,7 @@ class Command(BaseCommand):
                         write_error(error_file, "father_phone", a_student.studentid)
                 except Guardianstudent.DoesNotExist:
                     write_error(error_file, "No Father Listed", a_student.studentid)
-                try:
+                try:  # get mother info
                     a_mother = Guardianstudent.objects.filter(studentid=a_student.studentid, relationshipseq=2)[
                                :1].get()
                     mother_full_name = a_mother.personseq.firstname + ' ' + a_mother.personseq.lastname
@@ -142,15 +103,8 @@ class Command(BaseCommand):
                         write_error(error_file, 'mother_phone', a_student.studentid)
                 except Guardianstudent.DoesNotExist:
                     write_error(error_file, "No Mother Listed", a_student.studentid)
-                try:
-                    a_guardian = Guardianstudent.objects.filter(studentid=a_student.studentid,
-                                                                legalstatusseq=7).exclude(relationshipseq=32)[:1].get()
-                    a_guardianship = a_guardian.relationshipseq.relationshipdescr
-                    guardian_fn = a_guardian.personseq.firstname
-                    guardian_ln = a_guardian.personseq.lastname
-                except Guardianstudent.DoesNotExist:
-                    pass
-                try:
+
+                try:  # get the students guardian contact information
                     a_guardian = Guardianstudent.objects.filter(studentid=a_student.studentid, relationshipseq=32
                                                                 )[:1].get()
                     a_guardianship = a_guardian.relationshipseq.relationshipdescr
@@ -158,7 +112,7 @@ class Command(BaseCommand):
                     guardian_ln = a_guardian.personseq.lastname
                 except Guardianstudent.DoesNotExist:
                     pass
-                try:
+                try:  # emergance contacts
                     emerg_contact = Guardianstudent.objects.filter(studentid=a_student.studentid, relationshipseq=37)
                     if emerg_contact:
                         if emerg_contact[0]:
@@ -198,7 +152,7 @@ class Command(BaseCommand):
                                 pass
                 except Guardianstudent.DoesNotExist:
                     pass
-                #Getting student school code
+                #Getting student school code and next school code
                 if a_student.gradelevel == "12" or "11" or "10" or "09":
                     school_number = '2056112'
                     if a_student == '12':
@@ -206,17 +160,19 @@ class Command(BaseCommand):
                     else:
                         next_school_number = '2056112'
                 elif a_student.gradelevel == "07" or "08":
-                    if a_student.gradelevel == "08":
+                    if a_student.gradelevel == "08":#if grade 8 we set next school to highschool code
                         next_school_number = '2056112'
-                    try:
-                        t_school_number = Stuschoolenroll.objects.filter(studentid=a_student.studentid).order_by('buildingentrydate')[:1].get()
+                    try: # see what school middle school is
+                        t_school_number = Stuschoolenroll.objects.filter(
+                            studentid=a_student.studentid
+                        ).order_by('buildingentrydate')[:1].get()
                         if t_school_number.schoolprofileseq == 2:
                             school_number = '2055212'
                         elif t_school_number.schoolprofileseq == 3:
                             school_number = '2055112'
                         else:
                             school_number = 'gr78 no number listed'
-                        if a_student.gradelevel == "07":
+                        if a_student.gradelevel == "07":#if grade 7 we set next school to present school
                             next_school_number = school_number
                     except Stuschoolenroll.DoesNotExist:
                         pass
@@ -279,7 +235,7 @@ class Command(BaseCommand):
                               a_student.personseq.middleinitial, a_student.personseq.lastname, a_student.gradelevel,
                               a_student.gender, a_student_race, a_student.dateofbirth, a_student.ssn, entry_date,
                               "!---Exitdate---!", enroll_status,  next_school_number,
-                              Sched_Scheduled, a_student.originalyog, entry_code,
+                              sched_scheduled, a_student.originalyog, entry_code,
                               "!---TransferComment---!", district_entry_date, entry_sch_date,
                               home_address_street, student_address.addressseq.city, student_address.addressseq.state,
                               student_address.addressseq.zipcode, home_phone.phoneseq.phoneno, home_phone.phoneseq.phoneno,
