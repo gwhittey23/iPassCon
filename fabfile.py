fd7379349ac88fe4efb637b073d0ebd1c4155102
Gerard Whittey <gerard.whittey@gmail.com>
2013-11-29 20:29:36 -0500

    cleaned up code

--- fabfile.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'gerardwhittey'
-from fabric.api import local, cd
-
-
-def con_students(my_task):
-    code_dir = '/vagrant/'
-    with cd(code_dir):
-        local("./manage.py " + my_task + "  -a output")
-
